01a0143c474f0a6374956b1eb73e5c550bb85c34
Tatiit17 <tatianait17@gmail.com>
2024-02-21 18:38:56 +0100

    Added AI API

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-function generateRecipes(event) {
-	event.preventDefault();
-
+function displayRecipe(response) {
 	new Typewriter("#recipe", {
-		strings: "very delicious",
+		strings: response.data.answer,
 		autoStart: true,
 		delay: 1,
 		cursor: "",
 	});
+}
+function generateRecipes(event) {
+	event.preventDefault();
+
+	let recipeInput = document.querySelector("#recipe-ingredients");
+
+	let apiKey = "2a94c32a70f6060a6a93ft57413o5b0a";
+	let context =
+		"You are a gourman AI assistant, and you know super easy gluten-free recipes. You mission is to generate easy recipe in basic HTML. Make sure to follow the user instructions. Sign recipe with 'SheCodes AI' inside a <strong> element at the end of the recipe and NOT at the beginning";
+	let prompt = `User instructions: Generate a gluten-free recipe with the following ${recipeInput.value}`;
+	let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+	axios.get(apiUrl).then(displayRecipe);
 }
 
 let formElement = document.querySelector("#form-recipes-generator");
